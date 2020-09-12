f65133e7ff232a2d4b8a344b8fd414cd0cc587d7
isaacdb <66228532+isaacdb@users.noreply.github.com>
2020-09-11 23:55:46 -0300

    Commit

--- app/src/main/java/com/debiasi/primeiroaplicativo/imc/MainActivity.kt
+++ app/src/main/java/com/debiasi/primeiroaplicativo/imc/MainActivity.kt
@@ -3,10 +3,12 @@ package com.debiasi.primeiroaplicativo.imc
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<Button>(R.id.verificar).setOnClickListener {verificaIdade() }
         findViewById<Button>(R.id.go_imc).setOnClickListener {goScreenImc() }
+
     }
 
     private fun goScreenImc(){
@@ -29,6 +32,11 @@ class MainActivity : AppCompatActivity() {
         val idadeText = findViewById<EditText>(R.id.idade)
         val resultadoView = findViewById<TextView>(R.id.resultado)
 
+        val imcButton = findViewById<Button>(R.id.go_imc)
+        imcButton.visibility = View.GONE
+
+
+
         val idadeString = idadeText.text.toString().trim()
 
         if(idadeString != ""){
@@ -38,18 +46,21 @@ class MainActivity : AppCompatActivity() {
             if(idadeNumero>= 18){
                 resultadoView.text = "Acesso liberado, prossiga."
                 resultadoView.setTextColor(Color.parseColor("#008000"))
+                imcButton.visibility = View.VISIBLE
+
             }
             else{
                 resultadoView.text = "Você nao tem idade suficiente para este conteúdo."
                 resultadoView.setTextColor(Color.parseColor("#CC0000"))
+                imcButton.visibility = View.GONE
 
             }
         }
         else{
             resultadoView.text="Verificação de entrada por idade."
             resultadoView.setTextColor(Color.parseColor("#747474"))
+            imcButton.visibility = View.GONE
         }
 
     }
-
 }
--- app/src/main/java/com/debiasi/primeiroaplicativo/imc/SecondScreen.kt
+++ app/src/main/java/com/debiasi/primeiroaplicativo/imc/SecondScreen.kt
@@ -1,8 +1,10 @@
 package com.debiasi.primeiroaplicativo.imc
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -30,6 +32,7 @@ class SecondScreen : AppCompatActivity() {
         val alturaText = findViewById<EditText>(R.id.altura)
         val pesoText = findViewById<EditText>(R.id.peso)
         val resultadoImc = findViewById<TextView>(R.id.resultado)
+        val dica = findViewById<TextView>(R.id.dicas)
 
         val alturaString = alturaText.text.toString().trim()
         val pesoString = pesoText.text.toString().trim()
@@ -44,20 +47,36 @@ class SecondScreen : AppCompatActivity() {
 
             if(imc<18.5){
                 resultadoImc.text = "$imc Magreza"
+                resultadoImc.setTextColor(Color.parseColor("#FFD700"))
+                dica.text = "Voce está a baixo do peso ideal,busque um profissional adequado para verificar se sua esta sendo afetada de alguma maneira."
             }
             else if (imc>=18.5 && imc<25){
                 resultadoImc.text = "$imc Normal"
+                resultadoImc.setTextColor(Color.parseColor("#00FF00"))
+                dica.text = "Voce está no seu peso ideal, porem nao deixe de manter uma vida saudável!"
+
             }
             else if(imc >=25 && imc <30){
                 resultadoImc.text = "$imc Sobrepeso"
+                resultadoImc.setTextColor(Color.parseColor("#FF8C00"))
+                dica.text = "Voce está a cima do seu peso ideal, procure um profissional para lhe auxiliar a manter a saude, e evitar possiveis doenças."
             }
             else if(imc>=30 && imc < 40){
                 resultadoImc.text = "$imc Obesidade"
+                resultadoImc.setTextColor(Color.parseColor("#FF0000"))
+                dica.text = "Você esta em nivel de Obesidade, procure um especialista com urgência. Obesidade trazem doenças como diabetes, hipertensão, infarto e varios tipos de câncer."
+
             }
             else if (imc>=40){
                 resultadoImc.text = "$imc Obesidade grave"
-            }
+                resultadoImc.setTextColor(Color.parseColor("#8B0000"))
+                dica.text = "Você esta em nivel de Obesidade Morbida, grau extremamente perigoso, agravamento de varias doenças. Procure imediatamente um médico."
 
+            }
+            dica.visibility = View.VISIBLE
+        }
+        else{
+            dica.visibility = View.GONE
         }
 
     }
